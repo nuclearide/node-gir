f29e7738b62f7e6b21f439cb8d32c3a8bf0304ca
James Batt <getjamesbatt@gmail.com>
2017-10-28 14:18:46 +1100

    refactored return_value.test.js to use jest

--- __tests__/returned_value.test.js
+++ __tests__/returned_value.test.js
@@ -1,63 +1,51 @@
+const { load, Gtk } = require('../');
 
-var mocha = require('mocha'),
-    should = require('should');
+const GObject = load('GObject');
+const GdkPixbuf = load('GdkPixbuf');
 
-var TestGIR = require('./TestGIR');
-var win = TestGIR.win;
-var GdkPixbuf = TestGIR.GdkPixbuf;
-var GObject = TestGIR.GObject;
+const win = new Gtk.Window({
+    type: Gtk.WindowType.toplevel,
+    title: 'Node.JS GTK Window'
+});
 
-var pixbuf = new GdkPixbuf.Pixbuf(0, false, 1, 1, 1);
+const pixbuf = new GdkPixbuf.Pixbuf(0, false, 1, 1, 1);
 
 describe('Return value', function() {
-
     it('number', function() {
-        var int_value = GObject.type_from_name("GtkWindow");
-        int_value.should.be.a('number'); 
-    });
-
-    it('integer', function() {
-
+        const intValue = GObject.typeFromName('GtkWindow');
+        expect(typeof(intValue)).toEqual('number');
     });
 
     it('char', function() {
-        win.title = 'Lancelot';
-        win.title.should.be.a('string');
-    });
-
-    it('double', function() {
-
+        expect(win.title).toEqual('Lancelot');
+        expect(typeof(win.title)).toEqual('string');
     });
 
     it('boolean', function() {
-        win.get_decorated().should.be.a('boolean');
-    });
-
-    it('GValue', function() {
-
+        const decorated = win.getDecorated();
+        expect(typeof(decorated)).toEqual('boolean');
     });
 
     it('null', function() {
-        win.set_icon(null);
-        should.strictEqual(null, win.get_icon());
+        win.setIcon(null);
+        expect(win.getIcon()).toBeNull();
     });
 
     it('object', function() {
         win.icon = pixbuf;
-        win.icon.should.be.a('object');
+        expect(typeof(win.icon)).toEqual('object');
     });
 
     it('void', function() {
-        var void_value = win.resize(10, 10);
-        should.strictEqual(undefined, void_value);
+        const voidValue = win.resize(10, 10);
+        expect(voidValue).toBeUndefined();
     });
 
     describe('array', function() {
         it('GType', function() {
-            var type = GObject.type_from_name("GtkWindow");
-            var gtype_array = GObject.type_children(type);
-            gtype_array.length.should.not.equal(0);
+            const gtype = GObject.typeFromName('GtkWindow');
+            const gtypeArray = GObject.typeChildren(gtype);
+            expect(gtypeArray.length).not.toEqual(0);
         });
     });
 });
-
