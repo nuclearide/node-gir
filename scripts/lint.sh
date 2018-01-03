fce33e8f4f423afb5881e5a6711a57e0eef136ec
James Batt <getjamesbatt@gmail.com>
2018-01-03 19:30:47 +1100

    make scripts dir

---
+++ scripts/lint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+BASE_DIR=$(git rev-parse --show-toplevel)
+cd $BASE_DIR/build
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../
+$BASE_DIR/scripts/run-clang-tidy.py -header-filter='*' -checks='*'
