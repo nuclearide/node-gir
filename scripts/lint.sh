0ee77313ee121717635acade779709af3e16d777
James Batt <getjamesbatt@gmail.com>
2018-01-03 19:32:39 +1100

    added -fix to linter script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,4 +3,4 @@ set -e
 BASE_DIR=$(git rev-parse --show-toplevel)
 cd $BASE_DIR/build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../
-$BASE_DIR/scripts/run-clang-tidy.py -header-filter='*' -checks='*'
+$BASE_DIR/scripts/run-clang-tidy.py -header-filter='*' -checks='*' -fix
